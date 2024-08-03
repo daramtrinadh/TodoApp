25b177d32b4f8b4d32a58d08340b7b5c29ff694a
Daram Trinadh Reddy <daramtrinadh@gmail.com>
2024-08-03 15:39:19 +0530

    export issue

--- todo-frontend/src/app/ToDoItem.jsx
+++ todo-frontend/src/app/ToDoItem.jsx
@@ -1,10 +1,11 @@
-"use client"
+// components/ToDoItem.jsx
+"use client";
 
 import React, { useState } from "react";
 import EditModal from "./EditModal"; 
-import './ToDoItem.css'
+import './ToDoItem.css';
 
-const page = ({ eachTodo, onEditTodo, onDeleteTodo, onToggleComplete }) => {
+const ToDoItem = ({ eachTodo, onEditTodo, onDeleteTodo, onToggleComplete }) => {
     const { _id, text, completed } = eachTodo; 
     const [isEditing, setIsEditing] = useState(false);
     const [newText, setNewText] = useState(text);
@@ -38,13 +39,12 @@ const page = ({ eachTodo, onEditTodo, onDeleteTodo, onToggleComplete }) => {
             <div className="edit-delete-sec">
                 {/* Button to trigger edit mode */}
                 <button className="editBtn" onClick={() => setIsEditing(true)}>
-                    <svg height='1em' viewBox='0 0 512 512'>
-                        <path d='M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z'></path>
-                    </svg>
+                    Edit
                 </button>
                 {/* Button to delete the todo item */}
                 <button className="del-button" onClick={() => onDeleteTodo(_id)}>
-                            <svg
+                    {/* SVG for delete icon */}
+                    <svg
                         xmlns='http://www.w3.org/2000/svg'
                         fill='none'
                         viewBox='0 0 69 14'
@@ -57,22 +57,6 @@ const page = ({ eachTodo, onEditTodo, onDeleteTodo, onToggleComplete }) => {
                         <defs>
                         <clipPath id='clip0_35_24'>
                             <rect fill='white' height='14' width='69'></rect>
-                        </clipPath>
-                        </defs>
-                    </svg>
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 69 57'
-                        className='svgIcon bin-bottom'>
-                        <g clipPath='url(#clip0_35_22)'>
-                        <path
-                            fill='black'
-                            d='M20.8232 -16.3727L19.9948 -14.787C19.8224 -14.4569 19.4808 -14.25 19.1085 -14.25H4.92857C2.20246 -14.25 0 -12.1273 0 -9.5C0 -6.8727 2.20246 -4.75 4.92857 -4.75H64.0714C66.7975 -4.75 69 -6.8727 69 -9.5C69 -12.1273 66.7975 -14.25 64.0714 -14.25H49.8915C49.5192 -14.25 49.1776 -14.4569 49.0052 -14.787L48.1768 -16.3727C47.3451 -17.9906 45.6355 -19 43.7719 -19H25.2281C23.3645 -19 21.6549 -17.9906 20.8232 -16.3727ZM64.0023 1.0648C64.0397 0.4882 63.5822 0 63.0044 0H5.99556C5.4178 0 4.96025 0.4882 4.99766 1.0648L8.19375 50.3203C8.44018 54.0758 11.6746 57 15.5712 57H53.4288C57.3254 57 60.5598 54.0758 60.8062 50.3203L64.0023 1.0648Z'></path>
-                        </g>
-                        <defs>
-                        <clipPath id='clip0_35_22'>
-                            <rect fill='white' height='57' width='69'></rect>
                         </clipPath>
                         </defs>
                     </svg>
@@ -92,4 +76,4 @@ const page = ({ eachTodo, onEditTodo, onDeleteTodo, onToggleComplete }) => {
     );
 };
 
-export default page;
+export default ToDoItem;
